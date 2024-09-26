471dcb2e5c78c774ec3a4acd6962d973d106445a
Jheicer <jheicer.ro@gmail.com>
2024-09-26 10:14:47 -0500

    Inicio Rueditas FrontEnd

---
+++ src/main/java/pe/edu/cibertec/CL1_Rueditas_FrontEnd/Cl1RueditasFrontEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_Rueditas_FrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1RueditasFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1RueditasFrontEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL1_Rueditas_FrontEnd/Cl1RueditasFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_Rueditas_FrontEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1RueditasFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
